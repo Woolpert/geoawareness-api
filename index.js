c91eeec29ed17a8145866e86e866aea6dd7436c6
David Hollema <david.hollema@woolpert.com>
2020-09-01 11:55:31 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const repository = require('./src/repository');
+const ingestPublisher = require('./src/publisher');
+
+const { ErrorReporting } = require('@google-cloud/error-reporting');
+const errors = new ErrorReporting({ reportMode: 'always' });
+const wrap = fn => (...args) => fn(...args).catch(args[2]);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('REST API listening on port ', port);
+});
+
+app.get('/', wrap(async (req, res) => {
+    res.send('Welcome to GeoAwareness REST API');
+}));
+
+app.get('/stores', wrap(async (req, res) => {
+    try {
+        const stores = await repository.getStores();
+        res.json(stores);
+    } catch (error) {
+        next(error);
+    }
+}));
+
+app.get('/geofences', wrap(async (req, res) => {
+    try {
+        const storeName = req.query.storeName;
+        if (!storeName) {
+            throw ('Store name is required.');
+        }
+        const geofences = await repository.getGeofencesByStore(storeName);
+        res.json(geofences);
+    } catch (error) {
+        next(error);
+    }
+}));
+
+app.get('/orders', wrap(async (req, res, next) => {
+    try {
+        const storeName = req.query.storeName;
+        if (!storeName) {
+            throw ('Store name is required.');
+        }
+        const status = req.query.status;
+        const orders = await repository.getOrdersByStore(storeName, status);
+        res.json(orders);
+    } catch (error) {
+        next(error);
+    }
+}));
+
+app.patch('/orders/:orderId', wrap(async (req, res, next) => {
+    try {
+        const orderId = req.params.orderId;
+        if (!orderId) {
+            throw ('Order ID is required.');
+        }
+        let patch = req.body;
+
+        const storeName = patch.storeName;
+        if (!storeName) {
+            throw ('Store name not found in request body.');
+        }
+
+        let orderOriginal = await repository.getOrder(orderId, storeName);
+        if (!orderOriginal) {
+            throw (`Order ${orderId} not found for store ${storeName}.`);
+        }
+        const order = {
+            orderId: orderId,
+            ...orderOriginal,
+            ...patch
+        }
+
+        await repository.saveOrder(order);
+
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+}));
+
+app.post('/events', wrap(async (req, res, next) => {
+    try {
+        const evt = req.body;
+        console.log('event: ', evt);
+        ingestPublisher.publishMessage(evt);
+        res.status(201).send();
+    } catch (error) {
+        next(error);
+    }
+}));
+
+function errorHandler(err, req, res, next) {
+    errors.report(err);
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500);
+    res.json({ error: err });
+}
+app.use(errorHandler);
+app.use(errors.express);
---
+++ readme.md
@@ -0,0 +1,55 @@
+# GeoAwareness REST API
+
+The REST API provides access to the GeoAwareness data model and telemetry event ingest services.
+
+## Run Locally
+
+Running the API locally is useful for testing and debugging. Download the service account credentials created when standing up the [geoawareness-geofencing](https://github.com/woolpert-googlecloud/geoawareness-geofencing) service.
+
+```
+export GOOGLE_APPLICATION_CREDENTIALS=geoawareness-service-account-credentials.json
+
+nvm use
+npm install
+npm start
+```
+
+The API is available at [http://locahost:8080](http://localhost:8080).
+
+## Deploy to GCP
+
+### Backend Services
+
+```
+gcloud services enable servicemanagement.googleapis.com
+gcloud services enable servicecontrol.googleapis.com
+```
+
+```
+gcloud builds submit --tag gcr.io/$PROJECT_ID/geoawareness-backend
+gcloud run deploy geoawareness-backend --image gcr.io/$PROJECT_ID/geoawareness-backend --no-allow-unauthenticated --platform managed
+```
+
+### API
+
+#### Authentication
+
+[API key authentication](https://cloud.google.com/endpoints/docs/openapi/authentication-method#api_keys) is configured for Cloud Endpoints as specified in [geoawareness-api.yaml](./geoawareness-api.yaml#L17). This is a simple encrypted string and admittedly subject to man-in-the-middle-attacks. For a production deployment, we recommend implementing additional [authentication scheme(s)](https://cloud.google.com/endpoints/docs/openapi/authentication-method) that align with your existing client application authentication patterns. API key authentication alone is not enough if the API calls contain user data.
+
+#### Deploy
+
+The instructions [here](https://cloud.google.com/endpoints/docs/openapi/get-started-cloud-run#configure_es) to deploy the ESP to Cloud Run have been compiled into a build script. Everytime you modify and redeploy the Endpoints service configuration, you must re-run this entire step. Otherwise, changes to the service configuration will not be propagated to ESPv2 Beta.
+
+```
+sh build/build-deploy-api.sh <hostname> # Example: api.geoawareness.woolpert.dev
+```
+
+#### Map Custom Domain (Optional)
+
+See these [instructions](https://cloud.google.com/run/docs/mapping-custom-domains#command-line) to map a custom domain to the 'host' value in the service definition file.
+
+```
+gcloud beta run domain-mappings create --service geoawareness-api --domain <hostname> --platform managed # Example: api.geoawareness.woolpert.dev
+```
+
+Add [DNS records](https://cloud.google.com/run/docs/mapping-custom-domains#dns_update) at your domain registrar.
